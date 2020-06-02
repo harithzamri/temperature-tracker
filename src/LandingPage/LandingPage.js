2635596facaaea0f2092c76668e6cd9855fac36a
harithzamri <harithtoikee96@gmail.com>
2020-06-02 10:44:11 +0800

    first edit

---
+++ src/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './LandingPage.module.css'
+
+function LandingPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LandingPage
