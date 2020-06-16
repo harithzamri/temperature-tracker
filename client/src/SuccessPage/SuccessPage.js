0c240a9bf374e36d4ccdfeb84ed0780a30f78d19
harithzamri <harithtoikee96@gmail.com>
2020-06-16 10:28:11 +0800

    create successPage component

---
+++ client/src/SuccessPage/SuccessPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./SuccessPage.module.css";
+
+function SuccessPage() {
+  return (
+    <div className={styles["success"]}>
+      <span class="icon has-text-success">
+        <i className="fas fa-2x fa-check-square"></i>
+      </span>
+      <h2 className={`subtitle is-2 ${styles['title']}`}>Thank You</h2>
+      <h5 className="title is-5">Form has been successfully submitted</h5>
+    </div>
+  );
+}
+
+export default SuccessPage;
