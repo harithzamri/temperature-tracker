ff877a9e11ace1502ee8d3f9a439acffd4ba52a9
harithzamri <harithtoikee96@gmail.com>
2020-08-03 14:20:42 +0800

    edit on vendor data

--- client/src/History/DateHistory/VendorData/VendorData.js
+++ client/src/History/DateHistory/VendorData/VendorData.js
@@ -3,6 +3,7 @@ import styles from "./VendorData.module.css";
 import { useEffect } from "react";
 import { useState } from "react";
 import Axios from "axios";
+import { CSVLink } from "react-csv";
 
 function VendorData() {
   const [Vendor, setVendor] = useState([]);
@@ -11,10 +12,11 @@ function VendorData() {
     Axios.get("http://localhost:5000/vendor/getVendor").then((response) => {
       if (response.data.success) {
         console.log("vendor", response.data.vendor);
+        setVendor(response.data.vendor);
       }
     });
   }, []);
-  return <div>VendorData</div>;
+  return <CSVLink data={Vendor}>Vendor Data</CSVLink>;
 }
 
 export default VendorData;
